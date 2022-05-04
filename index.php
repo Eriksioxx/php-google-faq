a6bb8c0194d22500ab14570d840f600484765834
erik schievenin <erik.schievenin@gmail.com>
2022-05-04 18:32:52 +0200

    exercise complete

--- index.php
+++ index.php
@@ -12,7 +12,26 @@
     <title>Google FAQ</title>
 </head>
 <body>
+<header>
+    <div class="container">
+        <div class="left">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="Google Logo">
+            <span>Privacy e termini</span>
+        </div>
 
+        <div class="right">
+          <img src="avatar.jpg" alt="avatar">
+        </div>
+    </div>
+
+    <div class="hader-bottom">
+        <ul>
+          <?php foreach($menu as $link)
+            echo ('<li class="question">' . '<a href="#">' . $link['label'] .'<a href="#">'. '</li>')
+            ?>
+        </ul>
+      </div>
+</header>
 
     <main>
         <div class="faq">
