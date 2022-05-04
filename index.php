8cd204ff2a45b8970ff7af31e077edada858ac1e
erik schievenin <erik.schievenin@gmail.com>
2022-05-04 17:38:39 +0200

    Exercise complete

--- index.php
+++ index.php
@@ -2,16 +2,29 @@
     include('data/faq.php');
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="" href="styles.css">
+    <title>Google FAQ</title>
 </head>
 <body>
-    
+
+
+    <main>
+        <div class="faq">
+            
+            <?php foreach($faqs as $faq) {
+                echo('<div class="question">' . $faq['question'] . '</div>');
+                echo('<div class="answer">' . $faq['answer'] . '</div>');
+            }?>
+
+        </div>
+    </main>
+
+    <footer></footer>
 </body>
 </html>
